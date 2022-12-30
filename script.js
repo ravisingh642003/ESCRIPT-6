e950103a6aa9e1db6f2e5a309fd162bb62519e92
Ravi Singh <102274652+ravisingh642003@users.noreply.github.com>
2022-12-30 18:25:45 +0530

    challenge1

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-class UserCar {
-  constructor(CarBrand, CarModel) {
-    this.CarBrand = CarBrand
-    this.CarModel = CarModel
-  }
+var i = 10
+var j = 8
+var k = 6
 
-  getFullInfo() {
-    const val = document.getElementById('demo')
-    val.innerHTML = `I have a ${this.CarBrand} , it is a ${this.CarModel}`
-    val.style.color = 'teal'
-    val.style.fontSize = '30px'
-  }
+{
+  let i = 2
+
+  var j = 10
+
+  const k = 8
 }
-let obj = new UserCar('Ford', 'Mustang')
-obj.getFullInfo()
+
+const a = document.getElementById('demo1')
+a.innerHTML = 'The Value Of i is : ' + i
+a.style.color = 'teal'
+a.style.fontSize = '30px'
+document.write('<br>')
+const b = document.getElementById('demo2')
+b.innerHTML = 'The Value Of i is : ' + j
+b.style.color = 'tomato'
+b.style.fontSize = '30px'
+document.write('<br>')
+const c = document.getElementById('demo3')
+c.innerHTML = 'The Value Of i is : ' + k
+c.style.color = 'blue'
+c.style.fontSize = '30px'
